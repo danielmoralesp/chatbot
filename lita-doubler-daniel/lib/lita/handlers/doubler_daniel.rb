4c3e93566347dea79fe0e02e4e35350d27561bc9
danielmoralesp <daniel_moralesp@hotmail.com>
2019-02-03 19:23:26 -0500

    Route and Test

--- lita-doubler-daniel/lib/lita/handlers/doubler_daniel.rb
+++ lita-doubler-daniel/lib/lita/handlers/doubler_daniel.rb
@@ -2,6 +2,12 @@ module Lita
   module Handlers
     class DoublerDaniel < Handler
       # insert handler code here
+      route(
+        /^double\s+(\d+)$/i,
+        :respond_with_double,
+        command: true,
+        help: { 'double N' => 'prints N + N' }
+      )
 
       Lita.register_handler(self)
     end
--- lita-doubler-daniel/spec/lita/handlers/doubler_daniel_spec.rb
+++ lita-doubler-daniel/spec/lita/handlers/doubler_daniel_spec.rb
@@ -1,4 +1,13 @@
 require "spec_helper"
 
 describe Lita::Handlers::DoublerDaniel, lita_handler: true do
+  describe 'routing' do
+    it { is_expected.to route('Lita double 2') }
+    it { is_expected.to route('Lita double     22') }
+    it { is_expected.to route('Lita doUble 4') }
+
+    it { is_expected.to_not route('Lita double two') }
+    it { is_expected.to_not route('Lita double 1e4') }
+
+  end
 end
