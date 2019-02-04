17bee796e33adf0d79829ad923cbf8662d4a46e6
danielmoralesp <daniel_moralesp@hotmail.com>
2019-02-03 19:45:41 -0500

    Code of bot

--- lita-doubler-daniel/lib/lita/handlers/doubler_daniel.rb
+++ lita-doubler-daniel/lib/lita/handlers/doubler_daniel.rb
@@ -9,6 +9,18 @@ module Lita
         help: { 'double N' => 'prints N + N' }
       )
 
+      def respond_with_double(response)
+        # Read up on the Ruby MatchData class for more options
+        n = response.match_data.captures.first
+        n = Integer(n)
+
+        response.reply "#{n} + #{n} = #{double_number n}"
+      end
+
+      def double_number(n)
+        n + n
+      end
+
       Lita.register_handler(self)
     end
   end
